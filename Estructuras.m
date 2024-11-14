acea8590078fc4b6f15eac028de520f143563a6b
Paul-Ortiz <paul_ortiz02@hotmail.com>
2024-11-14 17:34:38 -0500

    Estructuras

---
+++ Estructuras.m
@@ -0,0 +1,22 @@
+%Read
+S =input("Ingrese un valor:" );
+%Write
+disp("Este es el resultado");
+
+%if then
+if 0
+ disp("Esta es la condicion verdadera")
+else
+disp("Esta es la condicion falsa")
+end
+
+%for
+
+for x=0:1:10
+disp(x);
+end
+x=1
+while x==1
+x=input("Ingrese X: ")
+
+end
---
+++ Estructuras.py
@@ -0,0 +1,27 @@
+#Read
+S =input("Ingrese un valor:" )
+#Write
+print("Este es el resultado")
+
+#if then
+if False:
+    print("Esta es la condicion verdera")
+else:
+    print("Esta es la condicion falsa")
+    
+#for
+vector ={"Pedro", "Pablo", "Jose"}  
+
+for x in vector:
+    print(x)
+
+for x in range(0,11):
+    print(x)
+
+#while
+x=1    
+while x==1:
+    x=input("Ingrese el valor de X: ")
+    x=int(x)
+    
+    
